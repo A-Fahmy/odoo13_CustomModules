9ba00d07d60e42d178bba771b550dfafb0b9e9aa
A-Fahmy <ahmed_Fahmy1982@yahoo.com>
2021-03-03 14:54:19 -0800

    update action_upload_done

--- CustomeModules/ditefacts/wizards/upload_product_for_users_meal.py
+++ CustomeModules/ditefacts/wizards/upload_product_for_users_meal.py
@@ -14,7 +14,7 @@ class upload_product_for_users_meal_wizard(models.TransientModel):
         return res
 
     def action_upload_done(self):
-        print('ahmed fahmy')
+        print('ahmed fahmy ahmed')
         # action_vals = {
         #     'name': _('users'),
         #     # 'domain': [('id', 'in', payments.ids), ('state', '=', 'posted')],
