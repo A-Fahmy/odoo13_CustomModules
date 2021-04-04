332b29fcedb70a2319e0111578c022a87f5296f9
A-Fahmy <ahmed_Fahmy1982@yahoo.com>
2021-04-04 14:59:30 -0700

    create Report Daily Sale by ORM

--- CustomeModules/ditefacts/reports/abstract_Report_Daily_meal.py
+++ CustomeModules/ditefacts/reports/abstract_Report_Daily_meal.py
@@ -5,7 +5,9 @@ import calendar
 from datetime import timedelta
 from dateutil.relativedelta import relativedelta
 from odoo import api,  models, _
-
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
+from datetime import datetime, timedelta
 class dailyMealReport(models.AbstractModel):
     _name = 'report.ditefacts.call_dailymealreport'
     _description = 'daily Meal Report'
@@ -14,12 +16,26 @@ class dailyMealReport(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         date_start = data['form']['date_start']
         date_end = data['form']['date_end']
-        docs = self.env['res.users.meal'].browse([23,29])
+        user_id = data['form']['user_id']
+        user_name = data['form']['user_name']
+        user_ids = data['form']['user_ids'][0]
+
+        start_date = datetime.strptime(date_start, DATE_FORMAT)
+        end_date = datetime.strptime(date_end, DATE_FORMAT)
+        domain = ['&', ('meal_date', '>=', start_date.strftime(DATETIME_FORMAT)), ('meal_date', '<', end_date.strftime(DATETIME_FORMAT))]
+        if user_id and user_id>0:
+            domain = domain + [('user_id', '=', user_id)]
+            # domain = domain + [('user_id', 'in', user_ids)]
+
+        docs = self.env['res.users.meal'].search(domain)
+        # docs = self.env['res.users.meal'].browse([23,29])
         print(docs)
         return {
             'doc_ids': self.ids,
             'doc_model': data['model'],
             'date_start': date_start,
             'date_end': date_end,
+            'user_id': user_id,
+            'user_name': user_name,
             'docs': docs,
         }
--- CustomeModules/ditefacts/wizards/Report_Daily_meal.py
+++ CustomeModules/ditefacts/wizards/Report_Daily_meal.py
@@ -8,6 +8,7 @@ class dailyMealReportWizard(models.TransientModel):
     date_start = fields.Date(string='Start Date', required=True, default=fields.Date.today)
     date_end = fields.Date(string='End Date', required=True, default=fields.Date.today)
     user_id = fields.Many2one('res.users', string="Meal User")
+    user_ids = fields.Many2many('res.users', string="Meal Users")
 
 
     def get_report(self):
@@ -15,10 +16,17 @@ class dailyMealReportWizard(models.TransientModel):
             'model': self._name,
             'ids': self.ids,
             'form': {
-                'date_start': self.date_start, 'date_end': self.date_end,
+                'date_start': self.date_start,
+                'date_end': self.date_end,
+                'user_id': self.user_id.id,
+                'user_name': self.user_id.name,
+                'user_ids': [tuple(self.user_ids.ids)],
+
             },
         }
 
         # ref `module_name.report_id` as reference.
+        # params = [tuple(self.user_ids.ids)]
+        # print(params)
 
         return self.env.ref('ditefacts.report_dailyMealReport').report_action(self, data=data)
